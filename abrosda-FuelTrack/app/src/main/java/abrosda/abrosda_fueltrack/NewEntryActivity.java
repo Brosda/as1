b6ae6761301b3ed983e02bbfc81ebc2166c38630
Alexander Brosda <abrosda@cs.ualberta.ca>
2016-01-27 13:28:17 -0700

    Wednesday

---
+++ abrosda-FuelTrack/app/src/main/java/abrosda/abrosda_fueltrack/NewEntryActivity.java
@@ -0,0 +1,52 @@
+package abrosda.abrosda_fueltrack;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class NewEntryActivity extends ActionBarActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_new_entry);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_entry, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void save(View view) {
+
+    }
+
+    public void cancel(View view) {
+
+    }
+}
